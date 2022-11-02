978949dd10281c66499eeceb56ace57b251cf992
Feynman Zhou <feynmanzhou@microsoft.com>
2022-11-02 18:40:12 +0800

    upgrade the notation-go and notation-core-go version
    Signed-off-by: Feynman Zhou <feynmanzhou@microsoft.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version shows the current notation-azure-kv version, optionally with pre-release.
-	Version = "v0.4.0-alpha.4"
+	Version = "v0.4.0-beta.1"
 
 	// BuildMetadata stores the build metadata.
 	BuildMetadata = "unreleased"
