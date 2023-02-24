64076c57ef0a2cf108c31a37d2692c798c94bf42
Junjie Gao <junjiegao@microsoft.com>
2023-02-24 13:56:01 +0800

    feat: remove autorest dependency
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/Azure/notation-azure-kv
 go 1.20
 
 require (
-	github.com/Azure/azure-sdk-for-go v67.0.0+incompatible
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.1.1
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.1
 	github.com/Azure/azure-sdk-for-go/sdk/keyvault/azcertificates v0.8.0
-	github.com/Azure/go-autorest/autorest v0.11.28
+	github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys v0.9.0
 	github.com/notaryproject/notation-core-go v1.0.0-rc.2
 	github.com/notaryproject/notation-go v1.0.0-rc.3
 	github.com/urfave/cli/v2 v2.23.6
@@ -14,25 +14,13 @@ require (
 )
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.1.1 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/keyvault/internal v0.7.0 // indirect
-	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
-	github.com/Azure/go-autorest/autorest/azure/auth v0.5.11 // indirect
-	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
-	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
-	github.com/Azure/go-autorest/logger v0.2.1 // indirect
-	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v0.8.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/dimchansky/utfbom v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/testify v1.7.2 // indirect
---
+++ internal/signature/key_test.go
@@ -0,0 +1,63 @@
+package signature
+
+import "testing"
+
+func Test_parseKeyID(t *testing.T) {
+	type args struct {
+		keyID string
+	}
+	tests := []struct {
+		name          string
+		args          args
+		wantVaultName string
+		wantDnsSuffix string
+		wantKeyName   string
+		wantSha       string
+		wantErr       bool
+	}{
+		{
+			name:          "valid keyID",
+			args:          args{keyID: "https://akvname.vault.azure.net/keys/keyname/b33b9e97ed0b4569b8cdede2162f4000"},
+			wantVaultName: "akvname",
+			wantDnsSuffix: "vault.azure.net",
+			wantKeyName:   "keyname",
+			wantSha:       "b33b9e97ed0b4569b8cdede2162f4000",
+			wantErr:       false,
+		},
+		{
+			name:          "valid keyID",
+			args:          args{keyID: "https://akvname.vault-test.azure.net/keys/keyname/b33b9e97ed0b4569b8cdede2162f4000"},
+			wantVaultName: "akvname",
+			wantDnsSuffix: "vault-test.azure.net",
+			wantKeyName:   "keyname",
+			wantSha:       "b33b9e97ed0b4569b8cdede2162f4000",
+			wantErr:       false,
+		},
+		{
+			name:    "invalid keyID",
+			args:    args{keyID: "http://akvname.vault.azure.net/keys/keyname/b33b9e97ed0b4569b8cdede2162f4000"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVaultName, gotDnsSuffix, gotKeyName, gotSha, err := parseKeyID(tt.args.keyID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseKeyID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotVaultName != tt.wantVaultName {
+				t.Errorf("parseKeyID() gotVaultName = %v, want %v", gotVaultName, tt.wantVaultName)
+			}
+			if gotDnsSuffix != tt.wantDnsSuffix {
+				t.Errorf("parseKeyID() gotDnsSuffix = %v, want %v", gotVaultName, tt.wantVaultName)
+			}
+			if gotKeyName != tt.wantKeyName {
+				t.Errorf("parseKeyID() gotKeyName = %v, want %v", gotKeyName, tt.wantKeyName)
+			}
+			if gotSha != tt.wantSha {
+				t.Errorf("parseKeyID() gotSha = %v, want %v", gotSha, tt.wantSha)
+			}
+		})
+	}
+}
