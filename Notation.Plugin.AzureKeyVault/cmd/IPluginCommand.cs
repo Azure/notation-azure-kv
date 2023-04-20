78903d742976b340906cd6226352b56a3cd116e1
Junjie Gao <junjiegao@microsoft.com>
2023-04-20 10:25:46 +0800

    fix: update code
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Notation.Plugin.AzureKeyVault/cmd/IPluginCommand.cs
+++
@@ -1,9 +0,0 @@
-namespace Notation.Plugin.AzureKeyVault.Cmd
-{
-    /// <summary>
-    /// Interface for plugin commands.
-    /// </summary>
-    public interface IPluginCommand{
-        Task<object> RunAsync(string inputJson);
-    }
-}
