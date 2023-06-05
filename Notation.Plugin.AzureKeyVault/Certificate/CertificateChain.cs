787d9ae52e66b7ba2cc9687d558505910b998d51
Junjie Gao <junjiegao@microsoft.com>
2023-06-05 14:37:45 +0800

    fix: remove X509Chain.Build to avoid compatibility issue (#117)
    After the PR, the plugin will not check the certificate chain integrity.
    
    Resolves #116 
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>
    
    ---------
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Notation.Plugin.AzureKeyVault/Certificate/CertificateChain.cs
+++ Notation.Plugin.AzureKeyVault/Certificate/CertificateChain.cs
@@ -13,45 +13,16 @@ namespace Notation.Plugin.AzureKeyVault.Certificate
         /// Build a certificate chain from a leaf certificate and a 
         /// certificate bundle.
         /// 
+        /// Note: the method doen't check the validity of the chain.
         /// <param name="certificateBundle">The certificate bundle.</param>
         /// <param name="leafCert">The leaf certificate.</param>
         /// <returns>A list of raw certificates in a chain.</returns>
         /// </summary>
         public static List<byte[]> Build(X509Certificate2 leafCert, X509Certificate2Collection certificateBundle)
         {
-            X509Chain chain = new X509Chain();
-            chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
-            chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-            chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
-            chain.ChainPolicy.CustomTrustStore.AddRange(certificateBundle);
-
-            try
-            {
-                bool isValid = chain.Build(leafCert);
-                if (!isValid)
-                {
-                    throw new ValidationException("Certificate is invalid");
-                }
-            }
-            catch (CryptographicException e)
-            {
-                throw new ValidationException($"Failed to build the X509 chain. {e.Message} The certificate bundle is unreadable. Please ensure the certificate bundle matches the specific certifcate.");
-            }
-
-            foreach (X509ChainStatus status in chain.ChainStatus)
-            {
-                if (status.Status == X509ChainStatusFlags.PartialChain)
-                {
-                    throw new ValidationException("Failed to build the X509 chain up to the root certificate. The provided certificate bundle either does not match or does not contain enough certificates to build a complete chain. To resolve this issue, provide the intermediate and root certificates by passing the certificate bundle file's path to the `ca_certs` key in the pluginConfig");
-                }
-
-                if (status.Status != X509ChainStatusFlags.NoError && status.Status != X509ChainStatusFlags.UntrustedRoot)
-                {
-                    throw new ValidationException($"Failed to build the X509 chain due to {status.StatusInformation}");
-                }
-            }
-
-            return chain.ChainElements.Select(x => x.Certificate.RawData).ToList();
+            X509Certificate2Collection chain = new X509Certificate2Collection(leafCert);
+            chain.AddRange(certificateBundle);
+            return chain.Select(x => x.RawData).ToList();
         }
     }
 }
