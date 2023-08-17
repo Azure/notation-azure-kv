1e625890cb1b603df72ea38da9ea8462c94a94f1
Junjie Gao <junjiegao@microsoft.com>
2023-08-17 11:08:10 +0800

    test: update
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Notation.Plugin.AzureKeyVault.Tests/Certificate/Pkcs12Tests.cs
+++ Notation.Plugin.AzureKeyVault.Tests/Certificate/Pkcs12Tests.cs
@@ -10,39 +10,39 @@ namespace Notation.Plugin.AzureKeyVault.Certificate.Tests
     public class Pkcs12Tests
     {
         [Fact]
-        public void RemoveMac()
+        public void ReEncode()
         {
             // read the pfx file
             byte[] data = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "TestData", "cert_chain.pfx"));
             Pkcs12Info originPfx = Pkcs12Info.Decode(data, out _);
             Assert.True(originPfx.IntegrityMode == Pkcs12IntegrityMode.Password);
 
-            // remove MAC for the pfx file
+            // re-encode the pfx file
             byte[] newData = Pkcs12.ReEncode(data);
             Pkcs12Info pfxWithoutMac = Pkcs12Info.Decode(newData, out _);
             Assert.True(pfxWithoutMac.IntegrityMode == Pkcs12IntegrityMode.None);
         }
 
         [Fact]
-        public void RemoveMac_WithInvalidMac()
+        public void ReEncode_WithInvalidMac()
         {
             // read the pfx file
             byte[] data = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "TestData", "cert_invalid_mac.pfx"));
             Pkcs12Info originPfx = Pkcs12Info.Decode(data, out _);
             Assert.True(originPfx.IntegrityMode == Pkcs12IntegrityMode.Password);
 
-            // decrypt the pfx file
+            // re-encode the pfx file
             Assert.Throws<ValidationException>(() => Pkcs12.ReEncode(data));
         }
 
         [Fact]
-        public void RemoveMac_withoutMac(){
+        public void ReEncode_withoutMac(){
             // read the pfx file
             byte[] data = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "TestData", "cert_without_mac.pfx"));
             Pkcs12Info originPfx = Pkcs12Info.Decode(data, out _);
             Assert.True(originPfx.IntegrityMode == Pkcs12IntegrityMode.None);
 
-            // remove MAC for the pfx file
+            // re-encode the pfx file
             byte[] newData = Pkcs12.ReEncode(data);
             Pkcs12Info pfxWithoutMac = Pkcs12Info.Decode(newData, out _);
             Assert.True(pfxWithoutMac.IntegrityMode == Pkcs12IntegrityMode.None);
