7e45e698dd3f997896f5f3d6e624108bcbbdc8dd
Junjie Gao <junjiegao@microsoft.com>
2023-09-01 14:16:01 +0800

    fix: macOS PKCS12 confidential mode = None & Integrity mode = Password(null) (#134)
    When import a PKCS12 certificate to AKV, it is possible that
    authenticatedSafe confidential mode = None & Integrity mode =
    Password(null)
    
    Resolves #133 
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>
    
    ---------
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Notation.Plugin.AzureKeyVault.Tests/Certificate/Pkcs12Tests.cs
+++ Notation.Plugin.AzureKeyVault.Tests/Certificate/Pkcs12Tests.cs
@@ -7,6 +7,7 @@ namespace Notation.Plugin.AzureKeyVault.Certificate.Tests
 {
     public class Pkcs12Tests
     {
+        // MAC integrity mode is password(null) and saftContent confidential mode is password(null)
         [Fact]
         public void ReEncode()
         {
@@ -34,7 +35,8 @@ namespace Notation.Plugin.AzureKeyVault.Certificate.Tests
         }
 
         [Fact]
-        public void ReEncode_withoutMac(){
+        public void ReEncode_withoutMac()
+        {
             // read the pfx file
             byte[] data = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "TestData", "cert_without_mac.pfx"));
             Pkcs12Info originPfx = Pkcs12Info.Decode(data, out _);
@@ -44,7 +46,21 @@ namespace Notation.Plugin.AzureKeyVault.Certificate.Tests
             byte[] newData = Pkcs12.ReEncode(data);
             Pkcs12Info pfxWithoutMac = Pkcs12Info.Decode(newData, out _);
             Assert.True(pfxWithoutMac.IntegrityMode == Pkcs12IntegrityMode.None);
+        }
 
+        // MAC integrity mode is password(null) and saftContent confidential mode is none
+        [Fact]
+        public void ReEncode_akv_imported()
+        {
+            // read the pfx file
+            byte[] data = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory(), "TestData", "akv_imported_cert.pfx"));
+            Pkcs12Info originPfx = Pkcs12Info.Decode(data, out _);
+            Assert.True(originPfx.IntegrityMode == Pkcs12IntegrityMode.Password);
+
+            // re-encode the pfx file
+            byte[] newData = Pkcs12.ReEncode(data);
+            Pkcs12Info pfxWithoutMac = Pkcs12Info.Decode(newData, out _);
+            Assert.True(pfxWithoutMac.IntegrityMode == Pkcs12IntegrityMode.None);
         }
     }
 }
