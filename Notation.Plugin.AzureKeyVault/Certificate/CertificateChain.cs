04ed6b123a82fd8c236eedd6222e0c98cbd76545
Junjie Gao <junjiegao@microsoft.com>
2023-09-04 14:44:49 +0800

    feat: build certificates path
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- Notation.Plugin.AzureKeyVault/Certificate/CertificateChain.cs
+++ Notation.Plugin.AzureKeyVault/Certificate/CertificateChain.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography.X509Certificates;
+using Notation.Plugin.Protocol;
 
 namespace Notation.Plugin.AzureKeyVault.Certificate
 {
@@ -8,19 +9,78 @@ namespace Notation.Plugin.AzureKeyVault.Certificate
     static class CertificateChain
     {
         /// <summary>
-        /// Build a certificate chain from a leaf certificate and a 
-        /// certificate bundle.
+        /// Build the certificate chain from the certificate bundle by matching
+        /// the issuer and subject distinguished name.
         /// 
-        /// Note: the method doen't check the validity of the chain.
-        /// <param name="certificateBundle">The certificate bundle.</param>
-        /// <param name="leafCert">The leaf certificate.</param>
-        /// <returns>A list of raw certificates in a chain.</returns>
+        /// It doesn't verify the certificate path.
         /// </summary>
-        public static List<byte[]> Build(X509Certificate2 leafCert, X509Certificate2Collection certificateBundle)
+        /// <param name="certs"></param>
+        /// <returns>
+        /// The certificate chain. The first certificate is the leaf certificate.
+        /// </returns>
+        /// <exception cref="PluginException"></exception>
+        public static X509Certificate2Collection Build(X509Certificate2Collection certs)
         {
-            X509Certificate2Collection chain = new X509Certificate2Collection(leafCert);
-            chain.AddRange(certificateBundle);
-            return chain.Select(x => x.RawData).ToList();
+            if (certs.Count == 0)
+            {
+                throw new PluginException("The certificate bundle is empty");
+            }
+
+            if (certs.Count == 1){
+                if (certs[0].SubjectName.Name != certs[0].IssuerName.Name)
+                {
+                    throw new PluginException("The certificate bundle only contains one certificate but it is not a self-signed certificate. Please complete the certificate bundle by `ca_certs` through plugin config.");
+                }
+                return certs;
+            }
+
+            // subject distinguished name -> certificate map
+            var certMap = new Dictionary<string, X509Certificate2>();
+            var issuerSet = new HashSet<string>();
+            foreach (var cert in certs)
+            {
+                if (certMap.ContainsKey(cert.SubjectName.Name))
+                {
+                    throw new PluginException($"The certificate bundle contains duplicated certificates: {cert.SubjectName.Name}");
+                }
+                certMap[cert.SubjectName.Name] = cert;
+                issuerSet.Add(cert.IssuerName.Name);
+            }
+
+            // count the leaf certificate
+            if (certs.Count(x => !issuerSet.Contains(x.SubjectName.Name)) != 1)
+            {
+                // AKV certificates always contain the leaf certificate
+                throw new PluginException("The certificate bundle may contains multiple leaf certificates");
+            }
+            var leafCert = certs.First(x => !issuerSet.Contains(x.SubjectName.Name));
+
+            // build the certificate chain
+            X509Certificate2Collection chain = new X509Certificate2Collection();
+            var currentCert = leafCert;
+            while (currentCert != null)
+            {
+                chain.Add(currentCert);
+
+                // root certificate
+                if (currentCert.SubjectName.Name == currentCert.IssuerName.Name)
+                {
+                    break;
+                }
+
+                // check if the issuer is found in the certificate bundle
+                if (!certMap.ContainsKey(currentCert.IssuerName.Name))
+                {
+                    throw new PluginException($"The certificate bundle is not complete. The issuer of {currentCert.SubjectName.Name} is not found.");
+                }
+                currentCert = certMap[currentCert.IssuerName.Name];
+            }
+
+            if (chain.Count != certs.Count)
+            {
+                throw new PluginException($"The certificate bundle has {certs.Count()} certificates but the certificate chain only has {chain.Count()} certficates.");
+            }
+            return chain;
         }
     }
 }
