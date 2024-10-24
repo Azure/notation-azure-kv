3fdb8e337242f756fdfa91fa188bc89fa3c633cc
Junjie Gao <junjiegao@microsoft.com>
2024-10-24 09:02:46 +0000

    fix: format
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- test/e2e/containerized/get_id_token/main.js
+++ test/e2e/containerized/get_id_token/main.js
@@ -1,20 +1,20 @@
-import * as core from '@actions/core'
-import * as fs from 'fs'
+import * as core from "@actions/core";
+import * as fs from "fs";
 
 async function getFederatedToken() {
-  const audience = 'api://AzureADTokenExchange'
-  const federatedToken = await core.getIDToken(audience)
-  return federatedToken
+  const audience = "api://AzureADTokenExchange";
+  const federatedToken = await core.getIDToken(audience);
+  return federatedToken;
 }
 
 async function main() {
   try {
-    const token = await getFederatedToken()
-    fs.writeFileSync("./federated_token", token)
-    console.log(`Federated Token written to ./federated_token`)
+    const token = await getFederatedToken();
+    fs.writeFileSync("./federated_token", token);
+    console.log(`Federated Token written to ./federated_token`);
   } catch (error) {
-    core.setFailed(`Action failed with error: ${error.message}`)
+    core.setFailed(`Action failed with error: ${error.message}`);
   }
 }
 
-main()
+main();
